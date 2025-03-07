4ec04988c50505d67a1d60d85828f7a072d8354e
LeoKorpi <leokorpi.game@gmail.com>
2025-03-07 12:35:19 +0100

    exercise 3.3 done

--- index.js
+++ index.js
@@ -32,6 +32,14 @@ app.get("/api/persons", (request, response) => {
   response.json(phonebookEntries);
 });
 
+app.get("/api/persons/:id", (request, response) => {
+  const id = request.params.id;
+  const person = phonebookEntries.find((person) => person.id === id);
+
+  if (person) response.json(person);
+  else response.status(404).end();
+});
+
 app.get("/info", (request, response) => {
   response.send(`
         <p>Phonebook has info for ${phonebookEntries.length} people</p>

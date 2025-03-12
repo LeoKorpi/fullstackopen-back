798b6a9a36ec950662be618da672159bff798145
LeoKorpi <leokorpi.game@gmail.com>
2025-03-12 22:16:16 +0100

    exercie 4.4 done

--- part4/tests/dummy.test.js
+++ part4/tests/dummy.test.js
@@ -8,3 +8,21 @@ test("dummy returns one", () => {
   const result = listHelper.dummy(blogs);
   assert.strictEqual(result, 1);
 });
+
+describe("total likes", () => {
+  const listWithOneBlog = [
+    {
+      _id: "5a422aa71b54a676234d17f8",
+      title: "Go To Statement Considered Harmful",
+      author: "Edsger W. Dijkstra",
+      url: "https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf",
+      likes: 5,
+      __v: 0,
+    },
+  ];
+
+  test("when a list only has one blog, the likes equals that blogs likes", () => {
+    const result = listHelper.totalLikes(listWithOneBlog);
+    assert.strictEqual(result, 5);
+  });
+});
--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -2,4 +2,10 @@ const dummy = (blogs) => {
   return 1;
 };
 
-module.exports = { dummy };
+const totalLikes = (blogs) => {
+  const reducer = (sum, blog) => sum + blog.likes;
+
+  return blogs.reduce(reducer, 0);
+};
+
+module.exports = { dummy, totalLikes };

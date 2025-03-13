6d2bc5744b0f05b99b331f1c78fc28a9876de754
LeoKorpi <leokorpi.game@gmail.com>
2025-03-13 20:03:18 +0100

    exercise 4.13 done

--- part4/tests/blog_api.test.js
+++ part4/tests/blog_api.test.js
@@ -1,4 +1,4 @@
-const { test, after, beforeEach } = require("node:test");
+const { test, after, beforeEach, describe } = require("node:test");
 const assert = require("node:assert");
 const mongoose = require("mongoose");
 const helper = require("./test_helper");
@@ -11,11 +11,7 @@ const Blog = require("../models/blog");
 
 beforeEach(async () => {
   await Blog.deleteMany({});
-
-  for (let blog of helper.initialBlogs) {
-    let blogObject = new Blog(blog);
-    await blogObject.save();
-  }
+  await Blog.insertMany(helper.initialBlogs);
 });
 
 test("blogs are returned as json", async () => {
@@ -38,79 +34,99 @@ test("the frist blog is about cooking as a student", async () => {
   assert(contents.includes("The student's kitchen"));
 });
 
-test("the unique identifier is named id, not _id", async () => {
-  const response = await api.get("/api/blogs");
+describe("viewing a specific blog", () => {
+  test("the unique identifier is named id, not _id", async () => {
+    const response = await api.get("/api/blogs");
 
-  assert("id" in response.body[0], "the blog should have an 'id' property");
-  assert(!("_id" in response.body[0]), "the blog should NOT have an '_id' property");
+    assert("id" in response.body[0], "the blog should have an 'id' property");
+    assert(!("_id" in response.body[0]), "the blog should NOT have an '_id' property");
+  });
 });
 
-test("a POST request successfully add a new blog to the db", async () => {
-  const newBlog = {
-    title: "Blog title",
-    author: "Name nameson",
-    url: "www.blog.com",
-    likes: 4,
-  };
+describe("Addition of a new blog", () => {
+  test("a POST request successfully add a new blog to the db", async () => {
+    const newBlog = {
+      title: "Blog title",
+      author: "Name nameson",
+      url: "www.blog.com",
+      likes: 4,
+    };
 
-  await api
-    .post("/api/blogs")
-    .send(newBlog)
-    .expect(201)
-    .expect("Content-Type", /application\/json/);
+    await api
+      .post("/api/blogs")
+      .send(newBlog)
+      .expect(201)
+      .expect("Content-Type", /application\/json/);
 
-  const response = await api.get("/api/blogs");
+    const response = await api.get("/api/blogs");
 
-  const contents = response.body.map((r) => r.title);
+    const contents = response.body.map((r) => r.title);
 
-  assert.strictEqual(response.body.length, helper.initialBlogs.length + 1);
-  assert(contents.includes("Blog title"));
+    assert.strictEqual(response.body.length, helper.initialBlogs.length + 1);
+    assert(contents.includes("Blog title"));
+  });
+
+  test("if likes property is missing, it defaults to 0", async () => {
+    const newBlog = {
+      title: "Nobody likes me...",
+      author: "Samuel sad",
+      url: "www.nolikes.com",
+      // likes is intentionally not included
+    };
+
+    const response = await api
+      .post("/api/blogs")
+      .send(newBlog)
+      .expect(201)
+      .expect("Content-Type", /application\/json/);
+
+    assert.strictEqual(response.body.likes, 0, "Expected default likes to be 0");
+  });
+
+  test("if title is missing, backend should respond with code 400", async () => {
+    const newBlog = {
+      author: "The person who forgets title and url to their",
+      url: "www.notitle.com",
+      likes: 3,
+      // Title are intentionally left out
+    };
+
+    const response = await api.post("/api/blogs").send(newBlog).expect(400);
+
+    assert.strictEqual(
+      response.body.error,
+      "Blog validation failed: title: Path `title` is required."
+    );
+  });
+
+  test("if url is missing, backend should respond with code 400", async () => {
+    const newBlog = {
+      title: "Url missing",
+      author: "The person who forgets title and url to their",
+      likes: 6,
+      // Url is intentionally left out
+    };
+
+    const response = await api.post("/api/blogs").send(newBlog).expect(400);
+
+    assert.strictEqual(response.body.error, "Blog validation failed: url: Path `url` is required.");
+  });
 });
 
-test("if likes property is missing, it defaults to 0", async () => {
-  const newBlog = {
-    title: "Nobody likes me...",
-    author: "Samuel sad",
-    url: "www.nolikes.com",
-    // likes is intentionally not included
-  };
+describe("Deletion of a blog", () => {
+  test("succeeds with code 204 if id is valid", async () => {
+    const blogsAtStart = await helper.blogsInDb();
+    const blogToDelete = blogsAtStart[0];
 
-  const response = await api
-    .post("/api/blogs")
-    .send(newBlog)
-    .expect(201)
-    .expect("Content-Type", /application\/json/);
+    await api.delete(`/api/blogs/${blogToDelete.id}`).expect(204);
 
-  assert.strictEqual(response.body.likes, 0, "Expected default likes to be 0");
-});
+    const blogsAtEnd = await helper.blogsInDb();
 
-test("if title is missing, backend should respond with code 400", async () => {
-  const newBlog = {
-    author: "The person who forgets title and url to their",
-    url: "www.notitle.com",
-    likes: 3,
-    // Title are intentionally left out
-  };
+    assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length - 1);
 
-  const response = await api.post("/api/blogs").send(newBlog).expect(400);
-
-  assert.strictEqual(
-    response.body.error,
-    "Blog validation failed: title: Path `title` is required."
-  );
-});
-
-test("if url is missing, backend should respond with code 400", async () => {
-  const newBlog = {
-    title: "Url missing",
-    author: "The person who forgets title and url to their",
-    likes: 6,
-    // Url is intentionally left out
-  };
-
-  const response = await api.post("/api/blogs").send(newBlog).expect(400);
-
-  assert.strictEqual(response.body.error, "Blog validation failed: url: Path `url` is required.");
+    const titles = blogsAtEnd.map((r) => r.title);
+    assert(!titles.includes(blogToDelete.title));
+  });
 });
 
 after(async () => {
--- part4/tests/test_helper.js
+++ part4/tests/test_helper.js
@@ -24,4 +24,17 @@ const initialBlogs = [
   },
 ];
 
-module.exports = { initialBlogs };
+const nonExistingId = async () => {
+  const blog = new Blog({ title: "willremovethissoon" });
+  await blog.save();
+  await blog.deleteOne();
+
+  return blog._id.toString();
+};
+
+const blogsInDb = async () => {
+  const blogs = await Blog.find({});
+  return blogs.map((blog) => blog.toJSON());
+};
+
+module.exports = { initialBlogs, nonExistingId, blogsInDb };
